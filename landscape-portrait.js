f990661dbdda25ab3e7c2ba253ce11053afd6fe3
david-forer <david.j.forer@gmail.com>
2021-08-15 14:18:03 +0900

    Inital Commit js Practice

---
+++ landscape-portrait.js
@@ -0,0 +1,7 @@
+let measurement = isLandscape(500, 300)
+console.log(measurement)
+
+
+function isLandscape(width, height) {
+return(width > height) ?  true : false
+}
---
+++ max of two numbers.js
@@ -0,0 +1,8 @@
+let number = maxTwo(6,10)
+console.log(number)
+
+function maxTwo(num1, num2) {
+    return (num1 > num2) ? num1 : num2
+
+}
+
