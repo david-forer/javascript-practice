584ea534e721315153942a42e93c91d8cd105e57
david-forer <david.j.forer@gmail.com>
2021-08-22 19:10:38 +0900

    calculator app with basic functionlity

---
+++ calculator/script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('calc-buttons'));
+
+buttons.map( button => {
+  button.addEventListener('click', (e) => {
+    switch(e.target.innerText){
+      case 'C':
+        display.innerText = '';
+        break;
+      case 'DEL':
+        if(display.innerText){
+          display.innerText = display.innerText.slice(0, -1)
+        }
+        break;
+      case '=':
+        try{
+          display.innerText = eval(display.innerText);
+      } catch {
+          display.innerText = "Error"
+      }
+        break;
+      default:
+                display.innerText += e.target.innerText;
+    }
+    // console.log('clicked');
+    // console.log(e);
+    console.log(e.target);
+    console.log(e.target.innerText)
+  })
+})
+console.log(buttons)
